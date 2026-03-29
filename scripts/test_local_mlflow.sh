22d04a9848faa97c294427a27a0649cd79887e1d
Hanif NIZAMUDDIN <hanif.nmh.01@gmail.com>
2026-03-29 18:15:11 +0200

    modif du fichier scripts/test_local_mlflow.sh

--- scripts/test_local_mlflow.sh
+++ scripts/test_local_mlflow.sh
@@ -1,7 +1,7 @@
-export KUBE_MLFLOW_TRACKING_URI=https://mlflow-$$$$-dev.apps.$$$$$.openshiftapps.com # <--- mettez ici l'url de votre service mlflow
-export MLFLOW_TRACKING_URI=https://mlflow-$$$$-dev.apps.$$$$.openshiftapps.com # <--- mettez ici l'url de votre service mlflow
-export MLFLOW_S3_ENDPOINT_URL=https://minio-api-$$$$-dev.apps.$$$$.openshiftapps.com # <--- mettez ici l'url de votre service minio
-export AWS_ACCESS_KEY_ID=$$$$ # <--- mettez ici le user minio
-export AWS_SECRET_ACCESS_KEY=$$$$ # <--- mettez ici le password minio
+export KUBE_MLFLOW_TRACKING_URI=https://mlflow-hanif0110-dev.apps.rm3.7wse.p1.openshiftapps.com/
+export MLFLOW_TRACKING_URI=https://mlflow-hanif0110-dev.apps.rm3.7wse.p1.openshiftapps.com/
+export MLFLOW_S3_ENDPOINT_URL=https://minio-api-hanif0110-dev.apps.rm3.7wse.p1.openshiftapps.com
+export AWS_ACCESS_KEY_ID=minio
+export AWS_SECRET_ACCESS_KEY=minio123
 
 uv run mlflow run ./src/titanic/training -e main --env-manager=local -P path=all_titanic.csv --experiment-name kto-titanic
